78e1109240e156d11af323fd0cd3b918a20ca837
asyakandyba <asyakan03@gmail.com>
2025-11-29 14:36:33 +0200

    Style

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,10 +1,10 @@
-const { NavLink, Link } = ReactRouterDOM
+const { NavLink } = ReactRouterDOM
 
 export function Home() {
   return (
     <section className="home">
       <img src="assets/img/home-bg.png" />
-      <div className="txt">
+      <div className="home-contect">
         <h2>Where Am I?</h2>
 
         <p>
@@ -23,25 +23,25 @@ export function Home() {
       </div>
       <section className="app-links flex">
 
-        <div className="books">
-          <div className="icon">
+        <div className="link">
+          <div className="link-img">
             <img src="assets/img/book.png" />
           </div>
           <h3>Books</h3>
         </div>
 
-        <div className="mail">
+        <div className="link">
           <NavLink to="/mail">
-            <div className="icon">
+            <div className="link-img">
               <img src="assets/img/email.png" />
             </div>
             <h3>Email</h3>
           </NavLink>
         </div>
 
-        <div className="notes">
+        <div className="link">
           <NavLink to="/note">
-            <div className="icon">
+            <div className="link-img">
               <img src="assets/img/notebook.png" />
             </div>
             <h3>Notes</h3>
